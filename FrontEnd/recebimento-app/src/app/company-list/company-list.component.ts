ae72081daa39ea440b8fcd673de4e1d88d38f7cf
Oliani <oliani748@gmail.com>
2024-06-17 21:21:04 -0300

    feature: create table and button to edit company

--- FrontEnd/recebimento-app/src/app/company-list/company-list.component.ts
+++ FrontEnd/recebimento-app/src/app/company-list/company-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { CompanyInterface } from '../interfaces/companyInterface';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-company-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, MatTableModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './company-list.component.html',
   styleUrl: './company-list.component.css'
 })
 export class CompanyListComponent {
+  supplier : Array<CompanyInterface> = [];
+  displayedColumns: string[] = ['name', 'date','actions'];
+
+  constructor(private service: AppService){}
+
+  ngOnInit(): void {
+    this.service.supplierList().subscribe( (supplier) => {
+      // supplier.forEach(element => {
+      //   element.receiptDate = element.receiptDate.toLocaleDateString('pt-BR');
+      // });
+      this.supplier = supplier;
+    });
+    console.log("GET: " + this.supplier);
+  }
+
+  // formatDateToString(date: Date){
+  //   console.log(date);
+  //   return new Date(date).toLocaleString('pt-BR', {timeZone: 'UTC'});
+  // }
 
 }
