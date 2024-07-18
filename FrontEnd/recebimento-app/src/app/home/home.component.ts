df55209708ea51ea9e8d15cec3bee6c972708b72
Oliani <oliani748@gmail.com>
2024-07-17 22:53:23 -0300

    feature: update schedule

--- FrontEnd/recebimento-app/src/app/home/home.component.ts
+++ FrontEnd/recebimento-app/src/app/home/home.component.ts
@@ -5,22 +5,27 @@ import { Observable } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { ScheduleInterface } from '../interfaces/scheduleInterface';
 import {MatTableModule} from '@angular/material/table';
-import { scheduler } from 'timers/promises';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CompanyComponent } from '../company/company.component';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule, MatTableModule],
+  imports: [HttpClientModule, MatTableModule, MatButtonModule, MatIconModule, MatTooltipModule, CompanyComponent, NgIf, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
   scheduler : Array<ScheduleInterface> = [];
-  displayedColumns: string[] = ['name', 'date'];
+  displayedColumns: string[] = ['name', 'date', 'actions'];
 
-  constructor(private service: AppService){}
+  constructor(private service: AppService, private router: Router, private route: ActivatedRoute){}
 
   ngOnInit(): void {
     this.service.scheduleList().subscribe( (schedule) => {
@@ -30,6 +35,11 @@ export class HomeComponent implements OnInit {
       this.scheduler = schedule;
     });
     console.log("GET: " + this.scheduler);
+  }
+
+  onEdit(schedule: ScheduleInterface){
+    console.log("edit: page(schedule-list) schedule.id: " + schedule.id);
+    this.router.navigate(['/agendamento', schedule.id], {relativeTo:this.route});
   }
 
   formatDateToString(date: Date){
--- FrontEnd/recebimento-app/src/app/interfaces/scheduleInterface.ts
+++ FrontEnd/recebimento-app/src/app/interfaces/scheduleInterface.ts
@@ -2,6 +2,7 @@ import { Time } from "@angular/common";
 import { CompanyInterface } from "./companyInterface";
 
 export interface ScheduleInterface{
+    id: number;
     supplier : CompanyInterface;
     fiscalNote : string;
     receiptDate: Date;
